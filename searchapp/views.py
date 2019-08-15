0ab3e2460035fd572c5440dbf372db1fa5f81992
ishanisri <ishasri.1212@gmail.com>
2019-08-15 20:45:09 +0530

    Generating test paper: Fixed after addition of board and grade

--- searchapp/views.py
+++ searchapp/views.py
@@ -55,9 +55,9 @@ def contact(request):
 
 
 def student_view(request):
-    subject_list = Subject.objects.all().values_list('subject_name', flat=True)
-    subject_list = list(subject_list)
-    return render(request, 'student_view.html', {'data': subject_list})
+    board_list = Board.objects.all().values_list('board', flat=True)
+    board_list = list(board_list)
+    return render(request, 'student_view.html', {'data': board_list})
 
 
 def login_view(request):
@@ -236,13 +236,10 @@ def get_chapters(request):
         subject = request.GET['subject']
         chapters = Chapter.objects.filter(
             subject__id=subject).values_list('id', 'chapter_name')
-        # papertype = request.GET['papertype']
         subject_breakup = SubjectSplit.objects.filter(
-            # name=papertype,
             subject__id=subject).values_list(
                 'id', 'name'
         ).distinct()
-        # print(subject_breakup)
         json_data = {
             'chapters': [{'chapter_id': x[0], 'chapter_name': x[1]} for x in chapters],
             'subject_breakup': [{'breakup_id': x[0], 'breakup_name': x[1]} for x in subject_breakup]
@@ -329,11 +326,15 @@ def delete_chapters(request):
 def get_test_paper(request):
     if request.method == 'GET':
         subject = request.GET['subject']
+        # board is used to decide the breakup of questions in test paper.
+        board = request.GET['board']
         chapters = request.GET.getlist('chapters[]')
-        papertype = request.GET['papertype']
+        # filter returns a query set which is converted to a list and then the first element is picked up.
+        subject_name = list(Subject.objects.filter(
+            id=subject).values_list('subject_name', flat=True))[0]
         subject_breakup = SubjectSplit.objects.filter(
-            name=papertype,
-            subject__subject_name__iexact=subject).values_list(
+            name=board,
+            subject__id=subject).values_list(
                 'question_weightage',
                 'question_type',
                 'total_questions',
@@ -354,7 +355,7 @@ def get_test_paper(request):
             token=token, submitted_date=datetime.datetime.now())
         generated_paper.save()
         generate_test_or_generic_paper(
-            subject, chapters, breakup, token, 'random')
+            subject_name, chapters, breakup, token, 'random')
         return JsonResponse({"message": "success", "token": token})
 
 
